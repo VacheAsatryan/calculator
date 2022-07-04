69cf6e2c62a0c257d1fa1f5cddab58d3aff61795
VacheAsatryan <vach.asatryan21@gmail.com>
2022-07-04 17:53:24 +0400

    change with beautify

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
 let out = document.getElementById('out');
 let num1,num2,action;
-let button = document.getElementById('button')
-button.addEventListener('click',()=>{
-num1=document.getElementById('n1').value
-num1=parseInt(num1)
-num2=document.getElementById('n2').value
-num2=parseInt(num2)
-action =document.getElementById('action').value
-switch(action){
-    case `+`:
-        out.innerHTML = num1 + num2
-        break;
-    case `-`:
-        out.innerHTML = num1 - num2
-        break;
-    case `/`:
-        out.innerHTML = num1 / num2
-        break;
-    case `*`:
-        out.innerHTML = num1 * num2
-        break;
-}
+let button = document.getElementById('button');
+
+button.addEventListener('click', () => {
+    num1=document.getElementById('n1').value;
+    num1=parseInt(num1);
+    num2=document.getElementById('n2').value;
+    num2=parseInt(num2);
+    action =document.getElementById('action').value;
+
+    switch(action){
+        case `+`:
+            out.innerHTML = num1 + num2
+            break;
+        case `-`:
+            out.innerHTML = num1 - num2
+            break;
+        case `/`:
+            out.innerHTML = num1 / num2
+            break;
+        case `*`:
+            out.innerHTML = num1 * num2
+            break;
+    }
 });
 
 
