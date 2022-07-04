9cad947910f694d160eb31522efe23e580f1ef9f
VacheAsatryan <vach.asatryan21@gmail.com>
2022-07-04 17:45:11 +0400

    change app.js

--- app.js
+++ app.js
@@ -1,32 +1,27 @@
- let out = document.getElementById('out');
- let num1,num2,action;
-
+let out = document.getElementById('out');
+let num1,num2,action;
 let button = document.getElementById('button')
- 
-
-function func(){
+button.addEventListener('click',()=>{
 num1=document.getElementById('n1').value
 num1=parseInt(num1)
 num2=document.getElementById('n2').value
 num2=parseInt(num2)
 action =document.getElementById('action').value
-
-if(action === `+`){
-    out.innerHTML= num1 + num2
-}else if(action === `-`){
-    out.innerHTML = num1 - num2
-}else if(action === `/`){
-    out.innerHTML = num1 / num2
-
-}else if(action === `*`){
-    out.innerHTML = num1 * num2
-
-}else{
-    alert(`error:no such action!`)
+switch(action){
+    case `+`:
+        out.innerHTML = num1 + num2
+        break;
+    case `-`:
+        out.innerHTML = num1 - num2
+        break;
+    case `/`:
+        out.innerHTML = num1 / num2
+        break;
+    case `*`:
+        out.innerHTML = num1 * num2
+        break;
 }
-
-
-}
+});
 
 
   
