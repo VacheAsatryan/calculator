4365700828410a8fd43a37496a6941f06693bf46
VacheAsatryan <vach.asatryan21@gmail.com>
2022-07-05 10:38:15 +0400

    change input

--- app.js
+++ app.js
@@ -1,29 +1,42 @@
 let out = document.getElementById('out');
-let num1,num2,action;
-let button = document.getElementById('button');
+let plus = document.getElementById('plus');
+let minus = document.getElementById('minus');
+let divide = document.getElementById('divide');
+let multiply = document.getElementById('multiply');
+let num1, num2;
 
-button.addEventListener('click', () => {
-    num1=document.getElementById('n1').value;
-    num1=parseInt(num1);
-    num2=document.getElementById('n2').value;
-    num2=parseInt(num2);
-    action =document.getElementById('action').value;
+plus.addEventListener('click', () => {
+    num1 = document.getElementById('n1').value;
+    num1 = parseInt(num1);
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(num2);
 
-    switch(action){
-        case `+`:
-            out.innerHTML = num1 + num2
-            break;
-        case `-`:
-            out.innerHTML = num1 - num2
-            break;
-        case `/`:
-            out.innerHTML = num1 / num2
-            break;
-        case `*`:
-            out.innerHTML = num1 * num2
-            break;
-    }
+    out.innerHTML = num1 + num2;
+});
+minus.addEventListener('click', () => {
+    num1 = document.getElementById('n1').value;
+    num1 = parseInt(num1);
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(num2);
+
+    out.innerHTML = num1 - num2;
+});
+divide.addEventListener('click', () => {
+    num1 = document.getElementById('n1').value;
+    num1 = parseInt(num1);
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(num2);
+
+    out.innerHTML = num1 / num2;
+});
+multiply.addEventListener('click', () => {
+    num1 = document.getElementById('n1').value;
+    num1 = parseInt(num1);
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(num2);
+
+    out.innerHTML = num1 * num2;
 });
 
 
-  
+
