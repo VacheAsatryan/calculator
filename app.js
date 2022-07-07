577eb5a9a8092b1e832e043b0a170375db43d166
VacheAsatryan <vach.asatryan21@gmail.com>
2022-07-07 14:50:26 +0400

    commited

--- app.js
+++ app.js
@@ -1,38 +1,37 @@
-let out = document.getElementById('out');
-let plus = document.getElementById('plus');
-let minus = document.getElementById('minus');
-let divide = document.getElementById('divide');
-let multiply = document.getElementById('multiply');
-let num1, num2;
+/* eslint-disable radix */
+const out = document.getElementById('out');
+const plus = document.getElementById('plus');
+const minus = document.getElementById('minus');
+const divide = document.getElementById('divide');
+const multiply = document.getElementById('multiply');
+let num1; let
+  num2;
 
 plus.addEventListener('click', () => {
-    num1 = document.getElementById('n1').value;
-    num1 = parseInt(num1);
-    num2 = document.getElementById('n2').value;
-    num2 = parseInt(num2);
-    out.innerHTML = num1 + num2;
+  num1 = document.getElementById('n1').value;
+  num1 = parseInt(num1);
+  num2 = document.getElementById('n2').value;
+  num2 = parseInt(num2);
+  out.innerHTML = num1 + num2;
 });
 minus.addEventListener('click', () => {
-    num1 = document.getElementById('n1').value;
-    num1 = parseInt(num1);
-    num2 = document.getElementById('n2').value;
-    num2 = parseInt(num2);
-    out.innerHTML = num1 - num2;
+  num1 = document.getElementById('n1').value;
+  num1 = parseInt(num1);
+  num2 = document.getElementById('n2').value;
+  num2 = parseInt(num2);
+  out.innerHTML = num1 - num2;
 });
 divide.addEventListener('click', () => {
-    num1 = document.getElementById('n1').value;
-    num1 = parseInt(num1);
-    num2 = document.getElementById('n2').value;
-    num2 = parseInt(num2);
-    out.innerHTML = num1 / num2;
+  num1 = document.getElementById('n1').value;
+  num1 = parseInt(num1);
+  num2 = document.getElementById('n2').value;
+  num2 = parseInt(num2);
+  out.innerHTML = num1 / num2;
 });
 multiply.addEventListener('click', () => {
-    num1 = document.getElementById('n1').value;
-    num1 = parseInt(num1);
-    num2 = document.getElementById('n2').value;
-    num2 = parseInt(num2);
-    out.innerHTML = num1 * num2;
+  num1 = document.getElementById('n1').value;
+  num1 = parseInt(num1);
+  num2 = document.getElementById('n2').value;
+  num2 = parseInt(num2);
+  out.innerHTML = num1 * num2;
 });
-
-
-
